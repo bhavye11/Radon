7bbed52290d252805fea2163e90afd2130e88781
Your Name <you@example.com>
2022-06-09 10:28:19 +0530

    Monday 6june assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const UserModel= require("../models/userModel")
+const bookModel = require('../models/bookModel')
 
 const createUser= async function (req, res) {
     let data= req.body
@@ -11,5 +12,57 @@ const getUsersData= async function (req, res) {
     res.send({msg: allUsers})
 }
 
+// solution 1st 
+
+const createBook = async function(req,res) {
+let Data = req.body;
+let saveData = await bookModel.create(Data);
+res.send({msg : saveData})
+}
+
+// solution 2nd 
+
+const booklist = async function(req,res) {
+    let allBooks = await bookModel.find().select({ bookName : 1, authorName :1, _id :0});
+    res.send({msg : allBooks})
+}
+
+// solution 3rd 
+
+const getBooksInYear = async function (req, res){
+    let myYear = req.body.year
+    let allBooks = await bookModel.find({myYear : year}).select({bookName :1, _id :0})
+    res.send({msg : allBooks})
+}
+
+// solution 4th 
+
+const getParticularBooks = async function (req, res){
+     let myData = req.body
+     let allBooks = await bookModel.find(myData)
+     res.send({msg : allBooks})
+}
+
+// solution 5th
+
+const getXINRBooks = async function (req, res){
+    let allBooks = await bookModel.find({"priceCodeBook.indianPrice" : {$in : ["600","450","500"]} }).select({bookName:1, _id:0})
+    res.send({msg : allBooks})
+}
+
+// solution 6th
+
+const getRandomBooks = async function (req, res){
+    let allBooks = await bookModel.find({ $or : [{totalPages : {$gt :500}}, {stockAvailable : true}]}).select({bookName : 1, _id : 0}).count()
+    res.send({msg : allBooks})
+
+}
+
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
+module.exports.createBook= createBook
+module.exports.booklist= booklist
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,44 @@
 const express = require('express');
+
 const router = express.Router();
+
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+
+const bookModel= require("../models/bookModel.js")
+
+const userController= require("../controllers/userController.js")
+
+//const BookController= require("../controllers/bookController")
+
+
 
 router.get("/test-me", function (req, res) {
+
     res.send("My first ever api!")
+
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+//router.post("/createUser", UserController.createUser  )
 
-router.get("/getBooksData", BookController.getBooksData)
+
+
+//router.get("/getUsersData", UserController.getUsersData)
+
+router.post("/createbook", userController.createBook)
+
+router.get("/bookList", userController.booklist)
+
+router.post("/getbooksinyear", userController.getBooksInYear)
+
+router.get("/getxINRbooks", userController.getXINRBooks)
+
+router.get("/getrandombooks", userController.getRandomBooks)
+
+router.get("/getParticularbooks", userController.getParticularBooks)
+
+
+
 
 module.exports = router;

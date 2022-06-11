d4ca99c03a2842f9d8da6e659e6a647adb49a7b6
Your Name <you@example.com>
2022-06-11 22:26:38 +0530

    10 june assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mmongodb+srv://bhavye11:Vdu3SztN9gdhFTvY@cluster0.syjbs.mongodb.net/bhavye1006-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
